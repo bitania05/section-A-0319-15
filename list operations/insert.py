f9a0f202016cb3a6a1d7b0e7bacca71836a259ba
bitania05 <abrahabitania@gmail.com>
2025-04-16 10:46:08 +0300

    updated

---
+++ list operations/insert.py
@@ -0,0 +1,7 @@
+# Create a list
+numbers = [1, 2, 3]
+
+# Insert an element at index 1 (second position)
+numbers.insert(1, 10)
+
+print(numbers)  # Output: [1, 10, 2, 3]
---
+++ list operations/remove.py
@@ -0,0 +1,8 @@
+# Create a list
+my_list = [1, 2, 3]
+
+# remove(): Removes the first occurrence of a specified value
+
+my_list.remove(2)
+
+print("After remove:", my_list)  # Output: [1, 10, 3, 4]
