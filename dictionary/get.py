286c09edd4190e30358398932a174d05b989f6ba
bitania05 <abrahabitania@gmail.com>
2025-04-26 09:47:37 +0300

    updated

---
+++ dictionary/get.py
@@ -0,0 +1,11 @@
+# Define a dictionary
+my_dict = {
+    'name': 'Alice',
+    'age': 25,
+    'city': 'New York'
+}
+
+# Using get() to access a value
+name = my_dict.get('name')  # returns 'Alice'
+print("Name:", name)
+
---
+++ dictionary/pop.py
@@ -0,0 +1,11 @@
+# Define a dictionary
+my_dict = {
+    'name': 'Alice',
+    'age': 25,
+    'city': 'New York'
+}
+
+
+# Using pop() to remove a key and get its value
+age = my_dict.pop('age')  # removes 'age' and returns 25
+print("Popped Age:", age)
